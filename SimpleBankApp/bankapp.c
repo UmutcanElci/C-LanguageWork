6a59a25385169588e0fdfb63c90aa186cbc52102
Umutcan Elçi <umutcanelci@yaani.com>
2024-12-25 21:33:29 +0300

    SimpleBankApp nearly finishned.

--- SimpleBankApp/bankapp.c
+++ SimpleBankApp/bankapp.c
@@ -31,8 +31,46 @@ int main(void) {
 
   printf("Great your information is saved\n");
 
+  account.balance = 100;
+
   while (1) {
     printf("Please select an option");
+    printf("1. Deposit\n");
+    printf("2. Withdraw\n");
+    printf("3. Check Balance\n");
+    printf("4. Exit\n");
+    int choice;
+
+    switch (choice) {
+    case1:
+      printf("Enter the amount to deposit: ");
+      float deposit;
+      scanf("%f", &deposit);
+      if (deposit <= 0) {
+        account.balance = deposit + account.balance;
+        printf("Your new balance : %f", account.balance);
+      } else {
+        printf("Your deposit value is not valid");
+      }
+      break;
+    case 2:
+      printf("Enter the amount to withdraw");
+      float withdraw;
+      scanf("%f", &withdraw);
+      if (account.balance < withdraw) {
+        printf("You can't withdraw this amount of money because your balance "
+               "is : %f",
+               account.balance);
+      } else {
+        account.balance = account.balance - withdraw;
+        printf("You withdraw %f amount of money new balance is %f", withdraw,
+               account.balance);
+      }
+      break;
+      case 3:
+
+    }
   }
+
   return 0;
 }
