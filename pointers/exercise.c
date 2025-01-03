dc747f8c5c1b5493aac2fb2187a8d407cb8d546b
Umutcan Elçi <umutcanelci@yaani.com>
2025-01-03 20:13:15 +0300

    First result of exercise 4 to sum two numbers

--- pointers/exercise.c
+++ pointers/exercise.c
@@ -1,34 +1,16 @@
 #include <stdio.h>
 
+int sumTwoNumbers(int *num1, int *num2) { return *num1 + *num2; }
+
 int main() {
-  int m = 300;
-  float fx = 300.600006;
-  char cht = 'z';
+  int a, b, sum;
 
-  // & address of
-  printf("address of m = %p\n", &m);
-  printf("address of fx = %p\n", &fx);
-  printf("address of cht = %p\n", &cht);
+  printf("First number : \n");
+  scanf("%d", &a);
+  printf("Second number : \n");
+  scanf("%d", &b);
 
-  // * value at address
-  printf("value at address of m = %d\n", *(&m));
-  printf("value at address of fx = %f\n", *(&fx));
-  printf("value at address of cht = %c\n", *(&cht));
+  sum = sumTwoNumbers(&a, &b);
 
-  int *pm;
-  float *pfx;
-  char *pcht;
-
-  pm = &m;
-  pfx = &fx;
-  pcht = &cht;
-  printf("address of m = %p\n", pm);
-  printf("address of fx = %p\n", pfx);
-  printf("address of cht = %p\n", pcht);
-
-  // Pointer to value
-
-  printf("value at address of m = %d\n", *pm);
-  printf("value at address of fx = %f\n", *pfx);
-  printf("value at address of cht = %c\n", *pcht);
+  printf("The sum of two numbers : %d", sum);
 }
