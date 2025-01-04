710deefde86a4e7b2ab6aa4a61abdd006daed950
Umutcan Elçi <umutcanelci@yaani.com>
2025-01-04 16:10:24 +0300

    Exercise 7 permutations of string

--- pointers/exercise.c
+++ pointers/exercise.c
@@ -1,26 +1,32 @@
 #include <stdio.h>
+#include <string.h>
 
-//& address of * value at address
+//& address of -  * value at address
+
+void swap(char *x, char *y) {
+  char temp = *x;
+  *x = *y;
+  *y = temp;
+}
+
+void permute(char *str, int start, int end) {
+  if (start == end) {
+    printf("%s  ", str);
+  } else {
+
+    for (int i = start; i <= end; i++) {
+      swap((str + start), (str + i));
+      permute(str, start+1, end);
+      swap((str + start), (str + i));
+    }
+  }
+}
 
 int main() {
-  int n;
-  printf("Input the size array : \n");
-  scanf("%d",&n);
 
-  int *ptr;
-  int arr[n];
-  
-  for (int i=0; i<n; i++) {
-    printf("Input the %d element : \n",i);
-    scanf("%d",&arr[i]);
-  }
+  char str[5] = "ABCDE";
 
+  int n = strlen(str);
 
-  ptr = arr;
-
-  printf("--------\n");
-  for (int j = 0; j < n; j++) {
-    printf("Element %d of the array : %d\n",j,*(ptr+j));
-  }
-
+  permute(str, 0, n - 1);
 }
