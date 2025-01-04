14fb3018e662f9a27a85e4c5da6ac10a0895f27f
Umutcan Elçi <umutcanelci@yaani.com>
2025-01-04 23:24:48 +0300

    Lenght of string with pointers - exercise 9

--- pointers/exercise.c
+++ pointers/exercise.c
@@ -1,23 +1,19 @@
 #include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
 
 //& address of -  * value at address
 
+int strLenght(char *str){
+  int len;
+
+  while (*str++) {
+    len++;
+  }
+  printf("%d",len);
+  return len;
+}
+
 int main() {
+  char a[10] = "Hello";
+  strLenght(a);
 
-  int n;
-  printf("Input total number of element (1 to 100) : ");
-  scanf("%d", &n);
-
-  int *ptr = (int *)malloc(n * sizeof(int));
-
-  for (int i = 0; i < n; i++) {
-    printf("\nNumber %d : ", i);
-    scanf("%d", ptr + i);
-  }
-
-  for (int i = 0; i < n; i++) {
-    printf("Your input Number =  %d  \n", *(ptr + i));
-  }
 }
