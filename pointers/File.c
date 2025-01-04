0009cb085f8c8f1d7b2ae4d175414157b60876f8
Umutcan Elçi <umutcanelci@yaani.com>
2025-01-04 23:45:07 +0300

    Learning File I/O with a simple apps

---
+++ pointers/File.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+void createTxt(char *name){}
+void writeTxt(){}
+void saveTxt(){}
+void closeTxt(){}
+
+
+int main(){
+  FILE *filePtr;
+
+  filePtr = fopen("Hello.txt", "w");
+}
+
