e061f5675b718844f96883d86b0b6fc2c35f8298
Umutcan Elçi <umutcanelci@yaani.com>
2025-01-05 19:49:51 +0300

    First try for making a simple txt file creation

--- pointers/File.c
+++ pointers/File.c
@@ -1,14 +1,34 @@
+#include <stddef.h>
 #include <stdio.h>
-
-void createTxt(char *name){}
-void writeTxt(){}
-void saveTxt(){}
-void closeTxt(){}
-
-
-int main(){
-  FILE *filePtr;
-
-  filePtr = fopen("Hello.txt", "w");
+FILE *filePtr;
+void createTxt(char *name) {
+  filePtr = fopen(name, "w");
+  fclose(filePtr);
 }
+void writeTxt(char *name) {
+  filePtr = fopen(name, "w");
+  char str[500];
+  printf("What you want to write here\n");
 
+  fgets(str, 500, stdin);
+
+  fprintf(filePtr, "%s", str);
+}
+void readTxt(char *name) {
+  filePtr = fopen(name, "r");
+  char str[500];
+  fgets(str, 500, filePtr);
+
+  printf("%s", str);
+}
+void saveTxt() {}
+void openTxt() {}
+void closeTxt() {}
+
+int main() {
+  char str[50] = "newFile.txt";
+  createTxt(str);
+  writeTxt(str);
+  readTxt(str);
+  return 0;
+}
