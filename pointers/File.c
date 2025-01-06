35de9fd5fde1a05ce6667f3d776f501fa348788d
Umutcan Elçi <umutcanelci@yaani.com>
2025-01-06 22:02:23 +0300

    Last stage of the File project

--- pointers/File.c
+++ pointers/File.c
@@ -1,40 +1,57 @@
-#include "io.h"
 #include <stddef.h>
 #include <stdio.h>
 #include <string.h>
-#include <unistd.h>
-
 
 #define MAX_TEXT_SIZE 256
 
 FILE *filePtr;
 char str[MAX_TEXT_SIZE];
 
-void createTxt(const char *name) { filePtr = fopen(name, "w"); }
+void createTxt(const char *name) {
+  filePtr = fopen(name, "w");
+  fclose(filePtr);
+}
 void writeTxt(const char *name) {
   filePtr = fopen(name, "w");
 
   printf("What you want to write here\n");
-
+  getchar();
   fgets(str, MAX_TEXT_SIZE, stdin);
-}
-
-void readTxt(const char *name) { filePtr = fopen(name, "r"); }
-
-void saveTxt() {
   fprintf(filePtr, "%s", str);
   fclose(filePtr);
 }
-void openTxt() {}
+
+void readTxt(const char *name) {
+  filePtr = fopen(name, "r");
+  printf("In txt file: \n");
+  fgets(str, MAX_TEXT_SIZE, filePtr);
+  printf("%s", str);
+  fclose(filePtr);
+}
+
 
 int main() {
-  // char str[30];
-  // printf("Enter the name of the file\n");
-  // fgets(str, 30, stdin);
+  char name[30];
+  printf("Enter the name of the file\n");
+  fgets(name, 30, stdin);
+  name[strcspn(name, "\n")] = '\0';
   //  add .txt to the file name
-  // strcat(str, ".txt");
-  char c;
-  while (read(STDIN_FILENO, &c, 1) == 1 && c != 'q');
-
-  return 0;
+  strcat(name, ".txt");
+  createTxt(name);
+  int choice;
+  while (1) {
+    printf("\nMenu:\n1. Write\n2. Read\n3. Exit\n");
+    printf("Enter your choice: ");
+    scanf("%d", &choice);
+    switch (choice) {
+    case 1:
+      writeTxt(str);
+      break;
+    case 2:
+      readTxt(str);
+      break;
+    case 3:
+      return 0;
+    }
+  }
 }
