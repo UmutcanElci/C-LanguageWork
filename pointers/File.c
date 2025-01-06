77d48ab5bd204bbf0f73b34403f01cb1f9d5f710
Umutcan Elçi <umutcanelci@yaani.com>
2025-01-06 18:59:38 +0300

    A little update for text editor app - adding different and new things...

--- pointers/File.c
+++ pointers/File.c
@@ -1,34 +1,40 @@
+#include "io.h"
 #include <stddef.h>
 #include <stdio.h>
+#include <string.h>
+#include <unistd.h>
+
+
+#define MAX_TEXT_SIZE 256
+
 FILE *filePtr;
-void createTxt(char *name) {
+char str[MAX_TEXT_SIZE];
+
+void createTxt(const char *name) { filePtr = fopen(name, "w"); }
+void writeTxt(const char *name) {
   filePtr = fopen(name, "w");
-  fclose(filePtr);
-}
-void writeTxt(char *name) {
-  filePtr = fopen(name, "w");
-  char str[500];
+
   printf("What you want to write here\n");
 
-  fgets(str, 500, stdin);
+  fgets(str, MAX_TEXT_SIZE, stdin);
+}
 
+void readTxt(const char *name) { filePtr = fopen(name, "r"); }
+
+void saveTxt() {
   fprintf(filePtr, "%s", str);
+  fclose(filePtr);
 }
-void readTxt(char *name) {
-  filePtr = fopen(name, "r");
-  char str[500];
-  fgets(str, 500, filePtr);
-
-  printf("%s", str);
-}
-void saveTxt() {}
 void openTxt() {}
-void closeTxt() {}
 
 int main() {
-  char str[50] = "newFile.txt";
-  createTxt(str);
-  writeTxt(str);
-  readTxt(str);
+  // char str[30];
+  // printf("Enter the name of the file\n");
+  // fgets(str, 30, stdin);
+  //  add .txt to the file name
+  // strcat(str, ".txt");
+  char c;
+  while (read(STDIN_FILENO, &c, 1) == 1 && c != 'q');
+
   return 0;
 }
