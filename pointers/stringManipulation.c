ddaa8121118fd55cb2349fe54aea3de3f1aee6d8
Umutcan Elçi <umutcanelci@yaani.com>
2025-01-08 21:52:30 +0300

    separate characters

--- pointers/stringManipulation.c
+++ pointers/stringManipulation.c
@@ -4,27 +4,13 @@
 int main() {
   char alphabet[] = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
 
-  // print size
-  printf("%d", strlen(alphabet));
-
-  // Combine two strings(Concatenate)
-
-  char str1[20] = "Hello ";
-  char str2[] = "World";
-
-  strcat(str1, str2);
-
-  printf("%s", str1);
-  // copy strings
-  char strc[20] = "Hi How are you?";
-  char strr[20];
-
-  strcpy(strr, strc);
-
-  printf("%s", strr);
-  // Compare strings
-  char str3[20] = "Hello World";
-
-  printf("%d\n", strcmp(str1, str2)); // returns -1 not equal
-  printf("%d\n", strcmp(str1, str3)); // returns 0 equal
+  // splitting
+  for (int i = 0; i < strlen(alphabet); i++) {
+    printf("%s",&alphabet[i]);
+    if (i < strlen(alphabet) - 1) {
+    printf(" ");
+    }
+  }
+  printf("\n");
+  return 0;
 }
