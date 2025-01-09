77792c5801302b5f9a7ae6f03a2b7db6014403a3
Umutcan Elçi <umutcanelci@yaani.com>
2025-01-09 18:36:09 +0300

    Exercise 2 - Merge two arrays also using with dynamic memory

--- DataStructures/Arrays.c
+++ DataStructures/Arrays.c
@@ -1,21 +1,46 @@
 #include <stdio.h>
+#include <stdlib.h>
 
 int main() {
-  printf("Input the size of the array : ");
-  int n;
-  scanf("%d", &n);
+  // Merge two array
+  int n1,n2;
+  printf("Input the size of first array : ");
+  scanf("%d",&n1);
+  puts("\n");
+  printf("Input the size of second array : ");
+  scanf("%d",&n2);
+  puts("\n");
+  
+  int arr1[n1],arr2[n2];
 
-  int arr[n];
-
-  int *ptr = arr;
-
-  puts("\n\n");
-  for (int i = 0; i < n; i++) {
-    printf("Please enter the array %d number : \n", i);
-
-    scanf("%d", ptr + i);
+  for (int i = 0; i < n1; i++) {
+    printf("Input the first array value %d : ",i);
+    scanf("%d",&arr1[i]);
   }
-  for (int i = 0; i < n; i++) {
-    printf("Array [%d] : %d \n", i, arr[i]);
-  }
+  puts("\n");
+ for (int i = 0; i < n2; i++) {
+  printf("Input the second array value %d : ",i);
+  scanf("%d",&arr2[i]);
+ }
+
+ puts("\n");
+
+ int *ptr = malloc(n1+n2);
+
+ for (int i =0; i < n1; i++) {
+ *(ptr + i) = arr1[i];
+ }
+ for (int i = 0; i < n2; i++) {
+   *(ptr + i + n1) = arr2[i];
+ }
+
+ printf("Merged array: ");
+    for (int i = 0; i < n1 + n2; i++) {
+        printf("%d ", *(ptr + i));
+    }
+    printf("\n");
+
+    free(ptr);
+
+    return 0;
 }
