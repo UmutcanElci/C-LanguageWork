dc2eb827524bf95274fc2c06b1699f7f10a26afc
Umutcan Elçi <umutcanelci@yaani.com>
2025-01-09 18:37:37 +0300

    Fix the format

--- DataStructures/Arrays.c
+++ DataStructures/Arrays.c
@@ -3,44 +3,44 @@
 
 int main() {
   // Merge two array
-  int n1,n2;
+  int n1, n2;
   printf("Input the size of first array : ");
-  scanf("%d",&n1);
+  scanf("%d", &n1);
   puts("\n");
   printf("Input the size of second array : ");
-  scanf("%d",&n2);
+  scanf("%d", &n2);
   puts("\n");
-  
-  int arr1[n1],arr2[n2];
+
+  int arr1[n1], arr2[n2];
 
   for (int i = 0; i < n1; i++) {
-    printf("Input the first array value %d : ",i);
-    scanf("%d",&arr1[i]);
+    printf("Input the first array value %d : ", i);
+    scanf("%d", &arr1[i]);
   }
   puts("\n");
- for (int i = 0; i < n2; i++) {
-  printf("Input the second array value %d : ",i);
-  scanf("%d",&arr2[i]);
- }
+  for (int i = 0; i < n2; i++) {
+    printf("Input the second array value %d : ", i);
+    scanf("%d", &arr2[i]);
+  }
 
- puts("\n");
+  puts("\n");
 
- int *ptr = malloc(n1+n2);
+  int *ptr = malloc(n1 + n2);
 
- for (int i =0; i < n1; i++) {
- *(ptr + i) = arr1[i];
- }
- for (int i = 0; i < n2; i++) {
-   *(ptr + i + n1) = arr2[i];
- }
+  for (int i = 0; i < n1; i++) {
+    *(ptr + i) = arr1[i];
+  }
+  for (int i = 0; i < n2; i++) {
+    *(ptr + i + n1) = arr2[i];
+  }
 
- printf("Merged array: ");
-    for (int i = 0; i < n1 + n2; i++) {
-        printf("%d ", *(ptr + i));
-    }
-    printf("\n");
+  printf("Merged array: ");
+  for (int i = 0; i < n1 + n2; i++) {
+    printf("%d ", *(ptr + i));
+  }
+  printf("\n");
 
-    free(ptr);
+  free(ptr);
 
-    return 0;
+  return 0;
 }
