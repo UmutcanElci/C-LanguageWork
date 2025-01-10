dd040f2bf535cc784d18235285c7078289074d2a
Umutcan Elçi <umutcanelci@yaani.com>
2025-01-10 21:27:22 +0300

    Adding LinkedList with input

--- DataStructures/LinkedList.c
+++ DataStructures/LinkedList.c
@@ -1,38 +1,76 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 struct node {
   int value;
   struct node *next;
 };
 
+struct node *head;
+
+void createNode(int numberOfNodes) {
+  struct node *newNode;
+  struct node *tempNode;
+
+  int nodeData;
+  int counter;
+
+  head = malloc(sizeof(struct node));
+
+  if (head == NULL) {
+    printf("Memory cannot be allocated");
+    return;
+  } else {
+
+    printf("Input the data for node %d :", counter);
+    scanf("%d", &nodeData);
+
+    head->value = nodeData;
+    head->next = NULL;
+
+    tempNode = head; // We need this to link the list
+
+    for (counter = 2; counter <= numberOfNodes; counter++) {
+      newNode = malloc(sizeof(struct node));
+
+      if (newNode == NULL) {
+        printf("Memory cannot be allocated");
+        return;
+      }
+      puts("\n");
+      printf("Input the data for node %d : ", counter);
+      scanf("%d", &nodeData);
+
+      newNode->value = nodeData;
+      newNode->next = NULL;
+      tempNode->next = newNode;
+      tempNode = newNode;
+    }
+  }
+}
+
 void printLinkedList(struct node *n) {
   while (n != NULL) {
-    printf("%d", n->value);
+    printf("%d ", n->value);
     n = n->next;
   }
 }
 
 int main() {
-  struct node *head;
-  struct node *one = NULL;
-  struct node *two = NULL;
-  struct node *three = NULL;
 
-  one = malloc(sizeof(struct node));
-  two = malloc(sizeof(struct node));
-  three = malloc(sizeof(struct node));
+  int n;
 
-  one->value = 1;
-  two->value = 2;
-  three->value = 3;
+  printf("Input the size of the node : ");
+  scanf("%d", &n);
+  puts("\n");
+  if (n <= 0) {
+    printf("The size of the linked list must be greater than 0.\n");
+    return 0;
+  }
 
-  one->next = two;
-  two->next = three;
-  three->next = NULL;
-
-  head = one;
-
+  createNode(n);
+  puts("\n");
   printLinkedList(head);
   return 0;
 }
