d0d21bd10c8aa0b04dc293d5b1ddb8c32083883a
Umutcan Elçi <umutcanelci@yaani.com>
2025-01-15 16:23:38 +0300

    With threads making a day simulation

--- Threads/daySimu.c
+++ Threads/daySimu.c
@@ -1,13 +1,48 @@
 #include <stdio.h>
 #include <pthread.h>
+#include <windows.h>
 
 #define YEAR 365
 #define DAY 24
 #define HOUR 60
 #define MINUTE 60
 
-void *daySimulation(); 
-
+void *daySimulation(void *arg); 
+int year = 0;
+int day = 0;
+int hour = 0;
+int minute = 0;
 int main(){
+  pthread_t thread;
 
+
+  pthread_create(&thread, NULL, &daySimulation, NULL);
+
+  pthread_join(thread, NULL);
+
+  return 0;
+
+}
+
+void *daySimulation(void *arg){
+  for(;;){
+    Sleep(1000);
+    minute++;
+    if(minute == MINUTE) {
+      minute = 0;
+      hour++;
+    }
+    if(hour == HOUR) {
+      hour = 0;
+      day++;
+    }
+    if(day == DAY) {
+      day = 0;
+      year++;
+    }
+
+    Sleep(1000);
+    printf("Year: %d, Day: %d, Hour: %d, Minute: %d\n", year, day, hour, minute);
+
+  }
 }
