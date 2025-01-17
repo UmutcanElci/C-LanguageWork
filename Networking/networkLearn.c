726d122db089877f53feed12a393f2c33bd8d2f3
Umutcan Elçi <umutcanelci@yaani.com>
2025-01-18 01:34:34 +0300

    Adding the logic of creating a socket and bind - taking things slower...

--- Networking/networkLearn.c
+++ Networking/networkLearn.c
@@ -1,6 +1,7 @@
 #include "corecrt.h"
 #include "io.h"
 #include "psdk_inc/_ip_types.h"
+#include <cerrno>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
@@ -8,81 +9,39 @@
 #include <winsock2.h>
 #include <ws2tcpip.h>
 
-#define PORT 8080
-#define BUFFER_SIZE 1024
 
 int main() {
-    WSADATA wsaData;
-    int server_fd, new_socket;
-    struct sockaddr_in address;
-    int opt = 1;
-    int addrlen = sizeof(address);
-    char buffer[BUFFER_SIZE] = {0};
+  int fd;
+struct sockaddr_in addr; 
 
-    // Initialize Winsock
-    if (WSAStartup(MAKEWORD(2, 2), &wsaData) != 0) {
-        fprintf(stderr, "WSAStartup failed with error: %d\n", WSAGetLastError());
-        exit(EXIT_FAILURE);
+  // IPv4 , TCP , 0
+  fd = socket(AF_INET, SOCK_STREAM, 0); // creating a socket
+  
+
+  // The Unix man pages list possible erors that can occur
+  // https://man7.org/linux/man-pages/man3/errno.3.html
+  if (fd == -1) {
+    switch (errno) {
+     case EPROTONOSUPPORT:
+       // Protocol Not Supported
+       perror("Protocol not Supported!");
+       break;
+     case EACCES:
+       //Permission denied
+       perror("Permission denied");
+       break;
+     default:
+       break;
     }
+  }
 
-    // Create socket
-    if ((server_fd = socket(AF_INET, SOCK_STREAM, 0)) == INVALID_SOCKET) {
-        fprintf(stderr, "Socket failed with error: %d\n", WSAGetLastError());
-        WSACleanup();
-        exit(EXIT_FAILURE);
-    }
+  //Bind a socket to a port 
+  // Socket , Pointer to addr strucutre , sizeof addr
+  if (bind(fd, (struct sockaddr *)&addr, sizeof(addr)) == -1) {
+    perror("Unable to bind!");
+  }
 
-    // Set socket options
-    if (setsockopt(server_fd, SOL_SOCKET, SO_REUSEADDR, (const char *)&opt, sizeof(opt)) != 0) {
-        fprintf(stderr, "setsockopt failed with error: %d\n", WSAGetLastError());
-        closesocket(server_fd);
-        WSACleanup();
-        exit(EXIT_FAILURE);
-    }
 
-    // Configure address
-    address.sin_family = AF_INET;
-    address.sin_addr.s_addr = INADDR_ANY;
-    address.sin_port = htons(PORT);
-
-    // Bind socket
-    if (bind(server_fd, (struct sockaddr *)&address, sizeof(address)) < 0) {
-        fprintf(stderr, "Bind failed with error: %d\n", WSAGetLastError());
-        closesocket(server_fd);
-        WSACleanup();
-        exit(EXIT_FAILURE);
-    }
-
-    // Listen for connections
-    if (listen(server_fd, 3) < 0) {
-        fprintf(stderr, "Listen failed with error: %d\n", WSAGetLastError());
-        closesocket(server_fd);
-        WSACleanup();
-        exit(EXIT_FAILURE);
-    }
-
-    printf("Server listening on port %d\n", PORT);
-
-    // Accept a connection
-    if ((new_socket = accept(server_fd, (struct sockaddr *)&address, (socklen_t *)&addrlen)) < 0) {
-        fprintf(stderr, "Accept failed with error: %d\n", WSAGetLastError());
-        closesocket(server_fd);
-        WSACleanup();
-        exit(EXIT_FAILURE);
-    }
-
-    printf("Connection Accepted\n");
-
-    ssize_t valread;
-    while ((valread = recv(new_socket, buffer, BUFFER_SIZE, 0)) > 0) {
-        printf("Client: %s", buffer);
-        memset(buffer, 0, sizeof(buffer));
-    }
-
-    closesocket(new_socket);
-    closesocket(server_fd);
-    WSACleanup();
-
-    return 0;
+  return 0;
 }
 
