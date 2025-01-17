1db80c1dbd33da7955a4c88227a798e3219a5d22
Umutcan Elçi <umutcanelci@yaani.com>
2025-01-18 02:25:21 +0300

    Writing and Reading data from user - okey not actually but close enough

--- Networking/networkLearn.c
+++ Networking/networkLearn.c
@@ -1,7 +1,7 @@
 #include "corecrt.h"
 #include "io.h"
 #include "psdk_inc/_ip_types.h"
-#include <cerrno>
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
@@ -9,9 +9,21 @@
 #include <winsock2.h>
 #include <ws2tcpip.h>
 
+#define BUFLEN 1500
+
 int main() {
+  WSADATA wsaData;
+  // Because I am using Windows
+  if (WSAStartup(MAKEWORD(2, 2), &wsaData) != 0) {
+    perror("WSAStartup failed");
+    exit(EXIT_FAILURE);
+  }
   int fd;
+
   struct sockaddr_in addr;
+
+  struct sockaddr_in cliaddr;
+  socklen_t cliaddrlen = sizeof(cliaddr);
 
   // IPv4 , TCP , 0
   fd = socket(AF_INET, SOCK_STREAM, 0); // creating a socket
@@ -33,6 +45,12 @@ int main() {
     }
   }
 
+  // Configure the server address structure
+
+  addr.sin_family = AF_INET;
+  addr.sin_port = htons(8080);
+  addr.sin_addr.s_addr = INADDR_ANY;
+
   // Bind a socket to a port
   //  Socket , Pointer to addr strucutre , sizeof addr
   if (bind(fd, (struct sockaddr *)&addr, sizeof(addr)) == -1) {
@@ -50,6 +68,41 @@ int main() {
   if (connect(fd, (struct sockaddr *)&addr, sizeof(addr)) == -1) {
     perror("Unable to open connection");
   }
+
+  // Accepting connections
+  // Creating a new File Descritor for user - taking data from user
+  int connfd = accept(fd, (struct sockaddr *)&cliaddr, &cliaddrlen);
+
+  if (connfd == -1) {
+    perror("Unablea to accept connection");
+  }
+
+  ssize_t i;
+  ssize_t rcount;
+  char buf[BUFLEN];
+  // Reading Data
+  rcount = read(fd, buf, BUFLEN);
+
+  if (rcount == -1) {
+    perror("Error while reading data");
+  }
+
+  for (i = 0; i < rcount; i++) {
+    printf("%c", buf[i]);
+  }
+
+  char data[] = "Hello I create a server I think...";
+  int datalen = strlen(data);
+
+  // Writing data
+  if (write(fd, data, datalen) == -1) {
+    perror("Error while writing data");
+  }
+
+  // Closing the Socket
+  close(connfd);
+  close(fd);
+  WSACleanup();
 
   return 0;
 }
