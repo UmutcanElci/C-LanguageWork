7f08e71122d996b94a0365f5f9bdbe2c04726002
Umutcan Elçi <umutcanelci@yaani.com>
2025-01-18 02:01:00 +0300

    Listening and Connecting to the server

--- Networking/networkLearn.c
+++ Networking/networkLearn.c
@@ -9,39 +9,47 @@
 #include <winsock2.h>
 #include <ws2tcpip.h>
 
-
 int main() {
   int fd;
-struct sockaddr_in addr; 
+  struct sockaddr_in addr;
 
   // IPv4 , TCP , 0
   fd = socket(AF_INET, SOCK_STREAM, 0); // creating a socket
-  
 
   // The Unix man pages list possible erors that can occur
   // https://man7.org/linux/man-pages/man3/errno.3.html
   if (fd == -1) {
     switch (errno) {
-     case EPROTONOSUPPORT:
-       // Protocol Not Supported
-       perror("Protocol not Supported!");
-       break;
-     case EACCES:
-       //Permission denied
-       perror("Permission denied");
-       break;
-     default:
-       break;
+    case EPROTONOSUPPORT:
+      // Protocol Not Supported
+      perror("Protocol not Supported!");
+      break;
+    case EACCES:
+      // Permission denied
+      perror("Permission denied");
+      break;
+    default:
+      break;
     }
   }
 
-  //Bind a socket to a port 
-  // Socket , Pointer to addr strucutre , sizeof addr
+  // Bind a socket to a port
+  //  Socket , Pointer to addr strucutre , sizeof addr
   if (bind(fd, (struct sockaddr *)&addr, sizeof(addr)) == -1) {
     perror("Unable to bind!");
   }
 
+  // Listening for connections
+  // socket , number of maximum conenctions(backlog)
+  if (listen(fd, 1) == -1) {
+    perror("Another errno... for listening");
+  }
+
+  // Connecting to a server
+  // Same as binding
+  if (connect(fd, (struct sockaddr *)&addr, sizeof(addr)) == -1) {
+    perror("Unable to open connection");
+  }
 
   return 0;
 }
-
